3759283a5e7c07386b28b8deb4b06f392c7994c2
Daminichandrakar <damini.2201@gmail.com>
2021-09-26 12:43:29 +0530

    Added: peakand pop operation on stack

--- StackTest.java
+++ StackTest.java
@@ -19,4 +19,18 @@ class StackTest {
 		Assert.assertEquals(myThirdNode, myNode);
 	}
 
+	@Test
+	public void given3NumbersInStackWhenPoppedShouldMatchWithLastAddedNode() {
+		Stack<Integer> stack = new Stack<>();
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		stack.push(myFirstNode);
+		stack.push(mySecondNode);
+		stack.push(myThirdNode);
+		INode<Integer> myNode = stack.pop();
+		stack.printStack();
+		Assert.assertEquals(myThirdNode, myNode);
+	}
+
 }
